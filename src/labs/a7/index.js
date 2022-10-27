bf302f44b18ad23f00442e6b03c72cad9bf161a3
maggie.li <maggie.yumeng.li@gmail.com>
2022-10-26 22:07:29 -0700

    added change to assignment 7

--- src/labs/a7/index.js
+++ src/labs/a7/index.js
@@ -1,11 +1,10 @@
+
 function Assignment7(){
     return(
-        <div>
+        <>
             <h2>Assignment 7</h2>
-            <h3>Exercise 1</h3>
-            <h3>Exercise 2</h3>
-            <h3>Exercise 3</h3>
-        </div>
+
+        </>
     )
 }
-export default Assignment7;
+// export default Assignment7;
